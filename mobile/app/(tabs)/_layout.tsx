dd71912e3df94c52f8c024761bd16cbf3c546387
gbatistaa <gbatistadev@gmail.com>
2026-02-12 13:41:00 -0300

    feat: Migrate to Expo Router, revamp UI components with new theming hooks and icon system, and update project structure and assets.

---
+++ mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,29 @@
+import { Tabs } from "expo-router";
+
+import { HapticTab } from "@/components/haptic-tab";
+import { IconSymbol } from "@/components/ui/icon-symbol";
+import { Colors } from "@/constants/theme";
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { Home } from "lucide-react-native";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+      }}
+    >
+      <Tabs.Screen
+        name="home/index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => <Home size={28} color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
